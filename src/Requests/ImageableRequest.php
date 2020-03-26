d3711c5c7aa9d83dc2c05f9d0f4c22bd6a1713b7
Jakub Gause <gause.jakub@gmail.com>
2020-03-26 07:03:15 +0000

    Apply fixes from StyleCI

--- src/Requests/ImageableRequest.php
+++ src/Requests/ImageableRequest.php
@@ -21,7 +21,7 @@ class ImageableRequest extends FormRequest
     }
 
     /**
-     * Creates and saves Image from Request
+     * Creates and saves Image from Request.
      *
      * @param \Illuminate\Database\Eloquent\Model $model
      * @return \Gause\ImageableLaravel\\Models\Image
@@ -49,9 +49,8 @@ class ImageableRequest extends FormRequest
         return $image;
     }
 
-
     /**
-     * Validate if Request is suitable for creating Image
+     * Validate if Request is suitable for creating Image.
      *
      * @return void
      */
