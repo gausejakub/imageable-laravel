f7a66975fcfdc037522afa1eb789b0f793c2b7eb
Jakub Gause <gause.jakub@gmail.com>
2020-03-27 21:36:53 +0100

    Imageable trait updated

--- src/Traits/Imageable.php
+++ src/Traits/Imageable.php
@@ -2,6 +2,12 @@
 
 namespace Gause\ImageableLaravel\Traits;
 
+use Gause\ImageableLaravel\Models\Image;
+
 trait Imageable
 {
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'model');
+    }
 }
---
+++ tests/Unit/Traits/HasImagesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Gause\ImageableLaravel\Tests\Unit\Traits;
+
+
+use Gause\ImageableLaravel\Tests\LaravelTestCase;
+use Gause\ImageableLaravel\Traits\Imageable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+class HasImagesTest extends LaravelTestCase
+{
+    /** @test */
+    public function trait_defines_images_relationship()
+    {
+        $model = new ImageableTraitDummyModel();
+
+        $this->assertInstanceOf(Relation::class, $model->images());
+    }
+}
+
+
+class ImageableTraitDummyModel extends Model
+{
+    use Imageable;
+}
